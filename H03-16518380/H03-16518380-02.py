4f3a617c9fd99b39a90610caecdb05a0b9650076
Morgen Sudyanto <moondemon68@gmail.com>
2018-10-09 21:59:29 +0700

    Update H03-16518380-02.py

--- H03-16518380/H03-16518380-02.py
+++ H03-16518380/H03-16518380-02.py
@@ -8,14 +8,14 @@ def prima(a):
     # KAMUS LOKAL
     # cnt,a: integer
     # ALGORITMA
-    cnt=0               #variabel untuk menghitung jumlah faktor dari a
+    cnt=0               # Variabel untuk menghitung jumlah faktor dari a
     for i in range(1,a+1):
         if (a%i==0):
-            cnt+=1;     #variabel cnt ditambah bila a habis membagi i
+            cnt+=1;     # Variabel cnt ditambah bila a habis membagi i
     if (cnt==2):
-        return True     #bilangan yang memiliki faktor sama dengan 2 adalah bilangan prima
+        return True     # Bilangan yang memiliki faktor sama dengan 2 adalah bilangan prima
     else:
-        return False    #bilangan yang memiliki faktor kurang dari atau lebih dari 2 bukan bilangan prima
+        return False    # Bilangan yang memiliki faktor kurang dari atau lebih dari 2 bukan bilangan prima
 
 def cek(a,b):
     # Menentukan apakah sepasang bilangan adalah sepasang bilangan PrimaPrima
@@ -23,15 +23,15 @@ def cek(a,b):
     # a,b: integer
     # ALGORITMA
     if (prima(a) and prima(b) and prima(10*a+b)):
-        return True     #bilangan PrimaPrima adalah bilangan yang a,b dan gabungannya(ab) nya prima
+        return True     # Bilangan PrimaPrima adalah bilangan yang a,b dan gabungannya(ab) nya prima
     else:
-        return False    #bilangan bukan PrimaPrima adalah bilangan yang a,b atau gabungannya(ab) nya bukan prima
+        return False    # Bilangan bukan PrimaPrima adalah bilangan yang a,b atau gabungannya(ab) nya bukan prima
 
 # ALGORITMA
-a=int(input("Masukkan A: "))    #input angka A
-b=int(input("Masukkan B: "))    #input angka B
+a=int(input("Masukkan A: "))    # Input angka A
+b=int(input("Masukkan B: "))    # Input angka B
 print("Pasangan bilangan PrimaPrima:")
 for i in range(a,b+1):
-    for j in range(a,b+1):                 #bilangan kedua harus lebih besar dari bilangan pertama
+    for j in range(a,b+1):                 # Bilangan kedua harus lebih besar dari bilangan pertama
         if (cek(i,j)):
-            print(str(i)+" "+str(j))       #mengoutputkan pasangan bilangan PrimaPrima
+            print(str(i)+" "+str(j))       # Mengoutputkan pasangan bilangan PrimaPrima
