216cbbf8e3bac819fa4c9a2a4fcb2aa3bd0479db
Morgen Sudyanto <moondemon68@gmail.com>
2018-10-09 21:59:57 +0700

    Update H03-16518380-03.py

--- H03-16518380/H03-16518380-03.py
+++ H03-16518380/H03-16518380-03.py
@@ -8,29 +8,29 @@ def fpb(a,b):
     # KAMUS LOKAL
     # mini,a,b: integer
     # ALGORITMA
-    mini=a                  #mini menyimpan angka yang lebih kecil dari a
+    mini=a                  # Mini menyimpan angka yang lebih kecil dari a
     if (b<mini):
-        mini=b              #mini mengubah bilangan menjadi b bila b lebih kecil dari mini (a)
+        mini=b              # Mini mengubah bilangan menjadi b bila b lebih kecil dari mini (a)
     for i in range(mini,0,-1):
         if (a%i==0 and b%i==0):
-            return i        #mengembalikan bilangan terbesar yang dapat membagi a dan b
+            return i        # Mengembalikan bilangan terbesar yang dapat membagi a dan b
 
 def kpk2(a,b):
     # Menentukan faktor persekutuan terbesar dari dua buah bilangan
     # KAMUS LOKAL
     # a,b: integer
     # ALGORITMA
-    return a*b//fpb(a,b)    #kpk dari 2 bilangan adalah hasil kali kedua bilangan tersebut dibagi fpbnya
+    return a*b//fpb(a,b)    # Kpk dari 2 bilangan adalah hasil kali kedua bilangan tersebut dibagi fpbnya
 
 def kpk3(a,b,c):
     # Menentukan faktor persekutuan terbesar dari dua buah bilangan
     # KAMUS LOKAL
     # a,b,c: integer
     # ALGORITMA
-    a=kpk2(a,b)             #variabel a diubah menjadi kpk dari a dan b
-    return kpk2(a,c)        #mengembalikan kpk dari a (kpk dari a dan b) dan c
+    a=kpk2(a,b)             # Variabel a diubah menjadi kpk dari a dan b
+    return kpk2(a,c)        # Mengembalikan kpk dari a (kpk dari a dan b) dan c
 
-a=int(input("Masukkan bilangan A: "))   #input angka a
-b=int(input("Masukkan bilangan B: "))   #input angka b
-c=int(input("Masukkan bilangan C: "))   #input angka c
-print("KPK dari "+str(a)+', '+str(b)+', dan '+str(c)+' adalah '+str(kpk3(a,b,c))+'.')   #mengoutputkan kpk dari a,b dan c
+a=int(input("Masukkan bilangan A: "))   # Input angka a
+b=int(input("Masukkan bilangan B: "))   # Input angka b
+c=int(input("Masukkan bilangan C: "))   # Input angka c
+print("KPK dari "+str(a)+', '+str(b)+', dan '+str(c)+' adalah '+str(kpk3(a,b,c))+'.')   # Mengoutputkan kpk dari a,b dan c
