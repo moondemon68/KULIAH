1c5394b0c791737e5cbed86da01b182dae6f35d0
Morgen Sudyanto <moondemon68@gmail.com>
2018-10-09 19:31:27 +0700

    Update H03-16518380-01.py

--- H03-16518380/H03-16518380-01.py
+++ H03-16518380/H03-16518380-01.py
@@ -1,4 +1,13 @@
+# Program Soal1
+# Menentukan apakah sepasang bilangan adalah pasangan bilangan KompositKomposit
+# KAMUS
+# a,b: integer
+
 def komposit(a):
+    # Menentukan apakah sebuah bilangan adalah bilangan komposit
+    # KAMUS LOKAL
+    # cnt,a: integer
+    # ALGORITMA
     cnt=0
     for i in range(1,a+1):
         if (a%i==0):
@@ -9,15 +18,20 @@ def komposit(a):
         return False
 
 def cek(a,b):
+    # Menentukan apakah sepasang bilangan adalah sepasang bilangan KompositKomposit
+    # KAMUS LOKAL
+    # a,b: integer
+    # ALGORITMA
     if (komposit(a) and komposit(b) and komposit(a+b)):
         return True
     else:
         return False
 
-a=int(input("Masukkan A: "))
-b=int(input("Masukkan B: "))
+# ALGORITMA
+a=int(input("Masukkan A: ")) #input angka A
+b=int(input("Masukkan B: ")) #input angka B
 print("Pasangan bilangan komposit:")
 for i in range(a,b+1):
     for j in range(i+1,b+1):
         if (cek(i,j)):
-            print(str(i)+" "+str(j))
+            print(str(i)+" "+str(j)) #mengoutputkan pasangan bilangan KompositKomposit
