9f95cb583f2581da8b886a6f0de365b7d68241f5
Morgen Sudyanto <moondemon68@gmail.com>
2018-10-31 11:42:24 +0700

    Update cobacoba.py

--- P05-16518380/cobacoba.py
+++ P05-16518380/cobacoba.py
@@ -4,7 +4,7 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 df=pd.read_csv("animal.csv")
-df.plot(kind="line",x="Year",y="Bears")
+df.plot(kind="line",x="Year",y="Bears",title="Line Chart")
 plt.savefig("line.png",bbox_inches='tight')
 
 df.plot(kind="bar",x="Year",y="Bears")
@@ -12,3 +12,28 @@ plt.savefig("bar.png",bbox_inches='tight')
 
 df.plot(kind="line",x="Year",y=["Bears","Dolphins","Whales"])
 plt.savefig("multiline.png",bbox_inches='tight')
+
+df.plot(kind="area",x="Year",y="Bears")
+plt.savefig("area.png",bbox_inches="tight")
+
+df.plot(kind="area",x="Year",y=["Bears","Dolphins","Whales"])
+plt.savefig("multiarea.png",bbox_inches="tight")
+
+df.plot(kind="scatter",x="Year",y="Bears")
+plt.savefig("scatter.png",bbox_inches="tight")
+
+df.plot(kind="scatter",x="Year",y="Bears",sizes=df["Bears"],color="red")
+plt.savefig("bubble.png",bbox_inches="tight")
+
+df2=df["Year"].value_counts()
+df2.plot(kind="pie")
+plt.savefig("pie.png",bbox_inches="tight")
+	
+df.plot(kind="bar",x="Year",y="Bears",title="Bar Chart")
+plt.savefig("bar.png",bbox_inches='tight')
+
+#df.groupby(["Year","Bears"]).size().unstack().plot(kind="line",title="Linegroupby")
+#plt.savefig("linegroupby.png",bbox_inches="tight")
+
+#df1=df.groupby(["Year","Bears"]).size().unstack()
+#print(df1)
